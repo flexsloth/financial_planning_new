da3a8ebad4dfaa7a19e9e338abce6a1d1e45ea0a
srinathbondala <srinath.b2004@gmail.com>
2024-02-01 18:39:10 +0530

    update

---
+++ app/src/main/java/com/example/myapplication/stock_tracker.java
@@ -0,0 +1,14 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class stock_tracker extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stock_tracker);
+    }
+}
