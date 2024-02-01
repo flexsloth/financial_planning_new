0dac23a85fd5ea39fb3f078693dbdbbee7a60ded
srinathbondala <srinath.b2004@gmail.com>
2024-02-01 21:18:01 +0530

    update1

---
+++ app/src/main/java/com/example/myapplication/calculator_classificationclass_java.java
@@ -0,0 +1,20 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class calculator_classificationclass_java extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view =inflater.inflate(R.layout.calculator_classificationclass,container,false);
+        //getChildFragmentManager().beginTransaction().replace(R.id.frameLayout,new records_activity()).commit();
+        return view;
+    }
+}
--- app/src/main/java/com/example/myapplication/main_menue.java
+++ app/src/main/java/com/example/myapplication/main_menue.java
@@ -1,6 +1,8 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -9,26 +11,49 @@ import android.widget.ImageView;
 
 public class main_menue extends AppCompatActivity implements View.OnClickListener {
 
-    private ImageView option,record_icon;
+    private ImageView option,record_icon,home_icon,calculator_icon,category_icon,analysis_icon;
+    private DrawerLayout drawerLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menue);
         getSupportActionBar().hide();
         option = (ImageView) findViewById(R.id.open_drawer_button);
+        drawerLayout = findViewById(R.id.drawer_layout1);
         record_icon = (ImageView) findViewById(R.id.record_icon);
+        analysis_icon=(ImageView)findViewById(R.id.analysis_icon);
+        home_icon = (ImageView) findViewById(R.id.home_icon);
+        calculator_icon = (ImageView) findViewById(R.id.calculator_icon);
+        category_icon =(ImageView)findViewById(R.id.category_icon);
+        home_icon.setOnClickListener(this);
+        calculator_icon.setOnClickListener(this);
+        category_icon.setOnClickListener(this);
         record_icon.setOnClickListener(this);
         option.setOnClickListener(this);
+        getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout,new home_profile_class()).commit();
     }
 
     @Override
     public void onClick(View view) {
         if(view==option){
-            Intent i = new Intent(getApplicationContext(),MainActivity.class);
-            startActivity(i);
+            drawerLayout.openDrawer(GravityCompat.START);
         }
         else if(view==record_icon){
             getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout,new records_activity()).commit();
+        }
+        else if(view==home_icon){
+            getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout,new home_profile_class()).commit();
+        }
+        else if(view==calculator_icon){
+           // getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout,new calculator_classificationclass_java()).commit();
+            Intent i = new Intent(getApplicationContext(),MainActivity.class);
+            startActivity(i);
+        }
+        else if(view==category_icon){
+            getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout,new catagery_class()).commit();
+        }
+        else if(view==analysis_icon){
+            getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout,new home_profile_class()).commit();
         }
     }
 }
